8f952ca590c72934c6c8d38403f2ba0df3d25381
Bryce Anderson <bryce.anderson22@gmail.com>
2016-02-03 12:47:06 -0500

    Now we can parse gaussian files too.
    A new command must be at the front, either orca or gaussian.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE FlexibleContexts           #-}
 
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-
-import OrcaEnergy.Parsing
+import OrcaEnergy.OrcaParsing as OP
+import qualified OrcaEnergy.GaussianParsing as GP
 import OrcaEnergy.Plotting
 import OrcaEnergy
 
@@ -25,21 +24,29 @@ defineFlag "f:first"  (4 :: Int)      "Energy levels before HOMO to plot"
 defineFlag "t:total"  (8 :: Int)      "Number of orbitals to plot"
 -}
 
+type Parser = FilePath -> IO [Orbital]
+
 data OptFlags = OptFlags { height :: Double
                          , first  :: Int
                          , total  :: Int
-                         , files  :: [String] }
-    deriving (Show, Eq)
+                         , files  :: [String]
+                         , parser :: Parser }
     
 parseArgs :: IO OptFlags 
 parseArgs = do
-  args <- getArgs
-  let go ("height":h:xs) Nothing f t = go xs (Just $ read h) f t
+  (program:args) <- getArgs
+  let prog = case program of 
+               "gaussian" -> GP.loadEnergies
+               "orca"     -> OP.loadEnergies
+               _          -> error $ "Invalid file format: " ++ program
+  
+      go ("height":h:xs) Nothing f t = go xs (Just $ read h) f t
       go ("first":f:xs) h Nothing t = go xs h (Just $ read f) t
       go ("total":t:xs) h f Nothing = go xs h f (Just $ read t)
-      go xs (Just h) (Just f) (Just t) = return $ OptFlags h f t xs
+      go xs (Just h) (Just f) (Just t) = return $ OptFlags h f t xs prog
       go _ _ _ _ = error $ "Failed to parse flags: " ++ show args
-      
+  
+  
   go args Nothing Nothing Nothing
     
   
@@ -60,8 +67,8 @@ findFiles =  filter (endswith ".out") `fmap` getDirectoryContents "."
 outFile = "energylevels.svg"
 
 runPlots :: OptFlags -> IO ()
-runPlots (OptFlags h f t fnames) = do
-  orbs <- loadOrbs f t `mapM` fnames
+runPlots (OptFlags h f t fnames parser) = do
+  orbs <- (\fp -> loadOrbs f t fp <$> parser fp) `mapM` fnames
   let maxWidth = (maximum $ width `map` orbs) :: Double
       total = foldl (\acc d -> acc ||| (d `atop` strutX maxWidth)) mempty orbs
       sz = mkHeight h
@@ -69,10 +76,14 @@ runPlots (OptFlags h f t fnames) = do
   putStrLn $ "Rendering to SVG " ++ show outFile
   renderSVG outFile sz total
   
---  loadOrbs :: 
-loadOrbs first total fp = do
-  engs <- loadEnergies fp
-  let os = takeOrbs first total engs
-      h  = text fp # fontSize (local 0.2) `atop` strutY 0.5
-  return $ h === plotOrbs os
+loadOrbs first total fp engs = h === plotOrbs os
+  where
+    os = takeOrbs first total engs
+    h  = text fp # fontSize (local 0.2) `atop` strutY 0.5
 
+takeOrbs :: Int -> Int -> [Orbital] -> [Orbital]
+takeOrbs homom cnt orbs = take cnt $ drop (occ - homom) orbs
+  where
+    occ = length $ takeWhile pred orbs
+    pred (Orbital _ EmptyOrb _) = False
+    pred _                      = True
---
+++ src/OrcaEnergy/GaussianParsing.hs
@@ -0,0 +1,48 @@
+module OrcaEnergy.GaussianParsing (
+    loadEnergies
+) where 
+
+import OrcaEnergy
+import System.FilePath
+import Debug.Trace
+import Data.String.Utils (splitWs, strip)
+import Data.List (sort, isPrefixOf)
+
+import Control.Applicative
+
+loadEnergies :: FilePath -> IO [Orbital]
+loadEnergies p = getOccupancy . lines <$> readFile p
+
+getOccupancy :: [String] -> [Orbital]
+getOccupancy ls = if null rst'' then alphaOrbs ++ betaOrbs 
+                  else trace "recursing" $ getOccupancy rst'' 
+  where
+    ls' = drop 1 $ dropWhile (not . isPrefixOf " The electronic state is") ls
+    (alphals,rst) = break (not . isPrefixOf " Alpha") ls'
+    (betals,rst') = break (isPrefixOf "  Beta") rst
+    -- look for the last set of energies defined
+    rst'' = dropWhile (not . isPrefixOf " The electronic state is") rst'
+  
+    alphaOrbs = concat $ parseLine (null betals) <$> alphals
+    betaOrbs  = concat $ parseLine False <$> betals
+  
+--  Alpha  occ. eigenvalues -- -276.14886-276.14876-276.14853-276.14846 -32.28936
+parseLine :: Bool -> String -> [Orbital]
+parseLine isRhf l = go (words header) where
+  (header,energies) = splitAt 24 l :: (String,String)
+  engs = take10 $ drop 4 energies
+  
+  go ["Alpha","occ.","eigenvalues"]  = (mkOrb alpha Occupied . read) <$> engs
+  go ["Alpha","virt.","eigenvalues"] = (mkOrb alpha EmptyOrb . read) <$> engs
+  go ["Beta","occ.","eigenvalues"]   = (mkOrb beta Occupied . read)  <$> engs
+  go ["Beta","virt.","eigenvalues"]  = (mkOrb beta EmptyOrb . read)  <$> engs
+  
+  take10 [] = []
+  take10 l = h:rs where
+    (h,t) = splitAt 10 l
+    rs = take10 t
+  
+  alpha = if isRhf then Nothing else Just Alpha
+  beta = Just Beta
+  
+  mkOrb s occ e = Orbital e occ s
