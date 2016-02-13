140e87a6728f7c389a68926c42f16983784762b8
Bryce Anderson <bryce.anderson22@gmail.com>
2016-02-12 22:05:33 -0500

    Start of adding an energy axis instead of labeling each orbital
    Not finished yet.

--- src/OrbitalEnergy.hs
+++ src/OrbitalEnergy.hs
@@ -11,13 +11,13 @@ module OrbitalEnergy (
 import Text.Printf
 
 import Diagrams.Core.Types (Renderable, Diagram)
-import Diagrams.TwoD.Text 
+import Diagrams.TwoD.Text
 import Diagrams.TwoD.Types (V2)
 import Diagrams.Path (Path)
 import Diagrams.Prelude
 
 import OrbitalEnergy.Plotting
-import qualified Debug.Trace
+import qualified Debug.Trace as T
 
 data Spin = Alpha
           | Beta
@@ -28,20 +28,13 @@ data OrbOcc = Occupied
      deriving (Eq, Show)
 
 data Orbital = Orbital { energy    :: Double
-                       , occupancy :: OrbOcc 
+                       , occupancy :: OrbOcc
                        , spin      :: Maybe Spin
                        } deriving (Show, Eq)
-
-
 
 instance Ord Orbital where
   compare a b = energy a `compare` energy b
 
-alphaBetaSpacing = 0.6
-vSpacingFactor = 25.0
-
-
--- (Renderable (Path V2 Double) b, Renderable (Text Double) b) => Orbital -> Diagram b R2
 plotOrb :: (Renderable (Path V2 Double) b, Renderable (Text Double) b) => Orbital -> QDiagram b V2 Double Any
 plotOrb o = p (occupancy o, spin o)
   where
@@ -49,8 +42,10 @@ plotOrb o = p (occupancy o, spin o)
     p (EmptyOrb, Nothing)    = addEnergy emptyLevel
     p (Occupied, Just Alpha) = addEnergy alpha # translate (r2 (-alphaBetaSpacing, 0.0))
     p (EmptyOrb, Just Alpha) = addEnergy emptyLevel # translate (r2 (-alphaBetaSpacing, 0.0))
-    p (Occupied, Just Beta)  = addEnergy beta  # translate (r2 (alphaBetaSpacing, 0.0)) 
-    p (EmptyOrb, Just Beta)  = addEnergy emptyLevel # translate (r2 (alphaBetaSpacing, 0.0)) 
+    p (Occupied, Just Beta)  = addEnergy beta  # translate (r2 (alphaBetaSpacing, 0.0))
+    p (EmptyOrb, Just Beta)  = addEnergy emptyLevel # translate (r2 (alphaBetaSpacing, 0.0))
+
+    alphaBetaSpacing = 0.6
 
     addEnergy :: Renderable (Text Double) b => QDiagram b V2 Double Any -> QDiagram b V2 Double Any
     addEnergy d = d <> t
@@ -60,18 +55,27 @@ plotOrb o = p (occupancy o, spin o)
         e = energy o
         t' = (text (printf "%0.2f eV" e) # fontSize (local 0.15)) `atop` strutX xshiftpos
         t = case spin o of
-              Just Alpha -> t' # translate (r2 (-xshiftpos, yshiftpos)) 
+              Just Alpha -> t' # translate (r2 (-xshiftpos, yshiftpos))
               _          -> t' # translate (r2 (xshiftpos, yshiftpos))
 
 -- plotOrbs :: (Backend b V2, Renderable (Path (V2 Double)) b, Renderable Text b) => [Orbital] -> Diagram b (V2 Double)
 plotOrbs :: (Renderable (Path V2 Double) b, Renderable (Text Double) b) => [Orbital] -> QDiagram b V2 Double Any
-plotOrbs os = foldl1 atop ds
+plotOrbs orbs = foldl1 atop (mapper <$> orbs) `atop` (plotAxis orbs # translate (r2 (-1.0,0)))
   where
-    c = length os
-    (b,t) = erange os
-    f e = b + e * vSpacingFactor / ((fromIntegral c) * (t-b)) 
-    mapper o = plotOrb o # translate (r2 (0.0, f (energy o)))
-    ds = mapper `map` os 
+    mapper orb = plotOrb orb # translate (r2 (0.0, position (energy orb)))
+    position eng = (eng - bottom) * scaleFactor
+    scaleFactor = height / (top - bottom)
+    (bottom,top) = energyBounds orbs
+    height = 1
 
-erange os = (minimum es, maximum es)
-  where es = map energy os
+plotAxis :: (Renderable (Path V2 Double) b, Renderable (Text Double) b) => [Orbital] -> QDiagram b V2 Double Any
+plotAxis orbs = (strokeLine engLine) # lw thin
+  where
+    offset = 0
+    engLine = lineFromVertices [ p2 (0,0), p2 (0,1.5)]
+
+
+energyBounds :: [Orbital] -> (Double,Double)
+energyBounds orbs = (minimum es, maximum es)
+  where es = map energy orbs
+
--- src/OrbitalEnergy/Plotting.hs
+++ src/OrbitalEnergy/Plotting.hs
@@ -7,8 +7,8 @@ module OrbitalEnergy.Plotting (
     alpha,
     beta,
     alphaBeta,
-    emptyLevel 
-) where 
+    emptyLevel
+) where
 
 import Diagrams.Coordinates
 import Diagrams.Prelude
@@ -17,21 +17,22 @@ import Diagrams.Util
 
 dashed = eLevel # dashingG [0.02,0.02] 0
 
--- alpha :: Renderable (Path V2 Double) b => Diagram R2
+alpha :: Renderable (Path V2 Double) b => QDiagram b V2 Double Any
 alpha = eLevel -- <> eArrow # lc red
 
--- beta :: Renderable (Path R2) b => Diagram b R2
+beta :: Renderable (Path V2 Double) b => QDiagram b V2 Double Any
 beta = eLevel -- <> eArrow # rotate (1/2 @@ turn) # lc blue
 
--- alphaBeta :: Renderable (Path R2) b => Diagram b R2
+alphaBeta :: Renderable (Path V2 Double) b => QDiagram b V2 Double Any
 alphaBeta = eLevel -- <> a # lc red <> b # lc blue
   where
     a = eArrow # translate (r2 (-0.1,0))
     b = a # rotate (1/2 @@ turn)
 
--- emptyLevel :: Renderable (Path R2) b => Diagram b R2
+emptyLevel :: Renderable (Path V2 Double) b => QDiagram b V2 Double Any
 emptyLevel = dashed -- eLevel
 
+eLevel :: Renderable (Path V2 Double) b => QDiagram b V2 Double Any
 eLevel = (strokeLine eLine) # lw thin # translate (r2 (-0.5,0.0))
   where
     eLine = lineFromVertices [ p2 (0,0), p2 (1,0)]
